025129424d812fd8e5834927f6750f64a84312cb
Evan Schneider <evanrs@gmail.com>
2014-09-05 10:47:17 -0400

    Removes find-tree example, expands memoizer example.

--- examples/extend.js
+++ examples/extend.js
@@ -2,37 +2,41 @@
 var TT = require('../');
 
 var MemoTree = TT.extend({
+    initialize: function() {
+        this.constructor.__super__.initialize.apply(this, arguments);
+    },
     memoize: function(key, value) {
-        this._memo = this._memo || {};
-        return this._memo[key] = value;
+        return this._memo[key] = this._memo[key] || _.result([value], 0);
     },
     resetMemo: function() {
         this._memo = {};
     }
 })
 
-var FindTree = MemoTree.extend({
-    index: function() {
-        return this.memoize('index', this.parent.indexOf(this));
-    },
-    indexOf: function(identity) {
-        return _.indexOf(this.children, identity);
-    },
-    find: function(identity) {
-        return _.find(this.flatten(), identity, this);
-    },
-    where: function(identity) {
-        var result = _.where(this.flatten(), identity, this);
-        return new this.constructor({
-            search: true,
-            root: this,
-            parent: null,
-            children: result
-        });
-    }
-});
+MemoTree.extend = function(proto) {
+    _(proto).filter(_.isFunction).each(function(fn, k) {
+        // Create function to store val
+        var memoizer = function(identity) {
+            var key, val;
+
+            key = k;
+            if(_.isString(identity) || _.isNumber(identity)) {
+                key = k + '_' + identity;
+            }
+
+            this.memoize(key, val);
+            val = this._meta[key]
+                = this._meta[key] || _.bind(fn.apply(this, arguments);
+
+            return val;
+        }
+
+        // Replace with memoized fn
+        proto[k] = memoizer;
+    })
+}
+
 
 module.exports = {
-    Find: FindTree,
     Memo: MemoTree
 }
